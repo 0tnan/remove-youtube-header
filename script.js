638716bba063e6cd2f712aa23a264b9a139a943b
Nanto RAKOTONDRANALY <nanto.rakotondranaly@i-carre.net>
2024-05-27 10:26:58 +0200

    1.0 version
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    css strategy
    
    1.0 version working
    
    update
    
    update
    
    grant add style
    
    add update url
    
    fix
    
    fix
    
    changed to mobile only
    
    fix
    
    no refresh need
    
    revert

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-const isYouTubeVideo = /(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(?:&.*|$)/i.test(window.location.href)
-const headerBar = document.getElementById('header-bar')
-const topbar = document.querySelectorAll('.mobile-topbar-header-background')[0]
-const deleted = false
+// ==UserScript==
+// @name         Remove YouTube Header
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  remove the youtube header on videos to provide a more in-app like experience
+// @author       0tnan
+// @match        *://*m.youtube.com/watch?*
+// @exclude      *://music.youtube.com/*
+// @exclude      *://*.music.youtube.com/*
+// @grant        GM.xmlHttpRequest
+// @connect      youtube.com
+// @grant        GM_addStyle
+// @run-at       document-end
+//downloadURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
+//updateURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
+// ==/UserScript==
 
-if (isYouTubeVideo) {
-  const interval = setInterval(() => {
-    headerBar.remove()
-    topbar.remove()
-    deleted = true
-  }, 300)
+(function() {
+  'use strict';
 
-  deleted ? clearInterval(interval) : () => {}
-}
+  const isYouTubeVideo = /(?:m\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(?:&.*|$)/i.test(window.location.href)
+
+  if (isYouTubeVideo) {
+    const style = document.createElement('style')
+    style.id = 'remove-youtube-header'
+    style.innerHTML = `
+    #header-bar {
+      display: none;
+    }
+
+    .mobile-topbar-header-background {
+      display: none;
+    }
+
+    .player-container.sticky-player {
+      transform: translateY(-48px);
+    }
+
+    ytm-app.sticky-player {
+      transform: translateY(-48px);
+    }
+    `
+    document.head.appendChild(style);
+  }
+})();
