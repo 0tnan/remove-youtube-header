a3caa8eab0ca7e9a46b5048ca4dddc35e4a0e2ac
Nanto RAKOTONDRANALY <nanto.rakotondranaly@i-carre.net>
2024-05-27 10:49:51 +0200

    revert

--- script.js
+++ script.js
@@ -22,46 +22,28 @@
 
   const isYouTubeVideo = /(?:m\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(?:&.*|$)/i.test(window.location.href)
 
-  function applyStyle() {
-    if (isYouTubeVideo) {
-      const style = document.createElement('style')
-      style.id = 'remove-youtube-header'
-      style.innerHTML = `
-      #header-bar {
-        display: none;
-      }
-  
-      .mobile-topbar-header-background {
-        display: none;
-      }
-  
-      .player-container.sticky-player {
-        transform: translateY(-48px);
-      }
-  
-      ytm-app.sticky-player {
-        transform: translateY(-48px);
-      }
-      `
-      document.head.appendChild(style);
-    } else {
-      document.getElementById('remove-youtube-header').remove()
+  if (isYouTubeVideo) {
+    const style = document.createElement('style')
+    style.id = 'remove-youtube-header'
+    style.innerHTML = `
+    #header-bar {
+      display: none;
     }
-  }
 
-  // Add a popstate event listener
-  window.addEventListener('popstate', function(event) {
-    if (event.state) {
-        console.log('Navigated to state: ' + JSON.stringify(event.state));
-        applyStyle()
-    } else {
-        console.log('No state: This could be the initial page load or a navigation to a URL without state.');
+    .mobile-topbar-header-background {
+      display: none;
     }
-  });
 
-  // Push initial state if needed
-  if (!history.state) {
-      history.replaceState({ page: 'initial' }, 'Initial Page', '');
-      console.log('Initial page load.');
+    .player-container.sticky-player {
+      transform: translateY(-48px);
+    }
+
+    ytm-app.sticky-player {
+      transform: translateY(-48px);
+    }
+    `
+    document.head.appendChild(style);
+  } else {
+    document.getElementById('remove-youtube-header').remove()
   }
 })();
