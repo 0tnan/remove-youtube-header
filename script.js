ecb92e62b0055541fc503904dce1c824bcb35ea7
Nanto RAKOTONDRANALY <nanto.rakotondranaly@i-carre.net>
2024-05-27 10:39:02 +0200

    fix

--- script.js
+++ script.js
@@ -4,15 +4,15 @@
 // @version      1.0
 // @description  remove the youtube header on videos to provide a more in-app like experience
 // @author       0tnan
-// @match        *://*m.youtube.com/watch?*
+// @match        *://*.youtube.com/*
 // @exclude      *://music.youtube.com/*
 // @exclude      *://*.music.youtube.com/*
 // @grant        GM.xmlHttpRequest
 // @connect      youtube.com
 // @grant        GM_addStyle
 // @run-at       document-end
-//downloadURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
-//updateURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
+// @downloadURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
+// @updateURL    https://raw.githubusercontent.com/0tnan/remove-youtube-header/main/script.js
 // ==/UserScript==
 
 (function() {
@@ -20,26 +20,46 @@
 
   const isYouTubeVideo = /(?:m\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(?:&.*|$)/i.test(window.location.href)
 
-  if (isYouTubeVideo) {
-    const style = document.createElement('style')
-    style.id = 'remove-youtube-header'
-    style.innerHTML = `
-    #header-bar {
-      display: none;
+  function applyStyle() {
+    if (isYouTubeVideo) {
+      const style = document.createElement('style')
+      style.id = 'remove-youtube-header'
+      style.innerHTML = `
+      #header-bar {
+        display: none;
+      }
+  
+      .mobile-topbar-header-background {
+        display: none;
+      }
+  
+      .player-container.sticky-player {
+        transform: translateY(-48px);
+      }
+  
+      ytm-app.sticky-player {
+        transform: translateY(-48px);
+      }
+      `
+      document.head.appendChild(style);
+    } else {
+      document.getElementById('remove-youtube-header').remove()
     }
+  }
 
-    .mobile-topbar-header-background {
-      display: none;
+  // Add a popstate event listener
+  window.addEventListener('popstate', function(event) {
+    if (event.state) {
+        console.log('Navigated to state: ' + JSON.stringify(event.state));
+        applyStyle()
+    } else {
+        console.log('No state: This could be the initial page load or a navigation to a URL without state.');
     }
+  });
 
-    .player-container.sticky-player {
-      transform: translateY(-48px);
-    }
-
-    ytm-app.sticky-player {
-      transform: translateY(-48px);
-    }
-    `
-    document.head.appendChild(style);
+  // Push initial state if needed
+  if (!history.state) {
+      history.replaceState({ page: 'initial' }, 'Initial Page', '');
+      console.log('Initial page load.');
   }
 })();
